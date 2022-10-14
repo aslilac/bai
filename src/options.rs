4c56507a3b87f1be7837c92fbf02e328fb1d3b9b
McKayla Washburn <mckayla@hey.com>
2022-10-14 02:31:38 -0600

    🧹

--- src/options.rs
+++ src/options.rs
@@ -1,15 +1,15 @@
 #[derive(Clone, Debug)]
 pub struct Options {
-    pub files: Vec<String>,
+	pub files: Vec<String>,
 }
 
 impl<S> FromIterator<S> for Options
 where
-    S: AsRef<str>,
+	S: AsRef<str>,
 {
-    fn from_iter<I: IntoIterator<Item = S>>(args: I) -> Self {
-        let args = args.into_iter();
-        let files = args.map(|arg| arg.as_ref().to_string()).collect();
-        Options { files }
-    }
+	fn from_iter<I: IntoIterator<Item = S>>(args: I) -> Self {
+		let args = args.into_iter();
+		let files = args.map(|arg| arg.as_ref().to_string()).collect();
+		Options { files }
+	}
 }
