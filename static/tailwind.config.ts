52644362a6814215af7cf0a6a824e9f16f361a9f
McKayla Washburn <mckayla@hey.com>
2025-03-08 23:13:03 -0700

    hello :)

--- static/tailwind.config.ts
+++ static/tailwind.config.ts
@@ -5,16 +5,6 @@ export default {
 			sans: ["Outfit", "sans-serif"],
 			serif: ["Merriweather", "serif"],
 			mono: ['"Cascadia Code"', "monoscape"],
-			hand: ["Reenie Beanie"],
-		},
-		extend: {
-			gridTemplateColumns: {
-				sidebar: "min-content auto",
-			},
-			transitionProperty: {
-				bg: "background-color",
-				spacing: "margin, padding",
-			},
 		},
 	},
 	plugins: [],
--- static/vite.config.ts
+++ static/vite.config.ts
@@ -1,8 +1,12 @@
 import ReactPlugin from "@vitejs/plugin-react";
+import TailwindPlugin from "@tailwindcss/vite";
 
 export default {
 	root: "src/",
-	plugins: [ReactPlugin({ babel: { plugins: ["react-compiler"] } })],
+	plugins: [
+		ReactPlugin({ babel: { plugins: ["react-compiler"] } }),
+		TailwindPlugin(),
+	],
 	build: {
 		emptyOutDir: true,
 		outDir: "../build/",
