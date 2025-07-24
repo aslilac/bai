d5f3883308bf756244dd53b9262955dcf7630709
McKayla Washburn <mckayla@hey.com>
2025-07-23 22:44:50 -0600

    update template files

--- static/vite.config.ts
+++ static/vite.config.ts
@@ -1,11 +1,13 @@
 import ReactPlugin from "@vitejs/plugin-react";
 import TailwindPlugin from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default {
 	root: "src/",
 	plugins: [
 		ReactPlugin({ babel: { plugins: ["react-compiler"] } }),
 		TailwindPlugin(),
+		tsconfigPaths(),
 	],
 	build: {
 		emptyOutDir: true,
